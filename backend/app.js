f954734b9a1e367a010ea2de932a4b2ee9bc839f
agraham02 <acgraham02@gmail.com>
2024-01-05 01:37:47 -0400

    Finish Plaid connection

--- backend/app.js
+++ backend/app.js
@@ -109,6 +109,24 @@ app.post("/plaid/create-link-token", async (req, res, next) => {
     }
 });
 
+app.post("/plaid/exchange_public_token", async (req, res, next) => {
+    const publicToken = req.body.public_token;
+    try {
+        const response = await client.itemPublicTokenExchange({
+            public_token: publicToken,
+        });
+
+        // These values should be saved to a persistent database and
+        // associated with the currently signed-in user
+        const accessToken = response.data.access_token;
+        const itemID = response.data.item_id;
+        res.json({ public_token_exchange: "complete" });
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+});
+
 app.listen(PORT || 3001, () => {
     console.log(`App listening on port ${PORT}`);
 });
---
+++ frontend/src/Components/InitializeLink.tsx
@@ -0,0 +1,66 @@
+// APP COMPONENT
+// Upon rendering of App component, make a request to create and
+// obtain a link token to be used in the Link component
+import React, { useEffect, useState } from "react";
+import { usePlaidLink } from "react-plaid-link";
+import { API_ENDPOINT } from "../utils";
+
+export default function InitializeLink() {
+    const [linkToken, setLinkToken] = useState(null);
+
+    async function generateToken() {
+        const response = await fetch(
+            `${API_ENDPOINT}/plaid/create-link-token`,
+            {
+                method: "POST",
+            }
+        );
+        const data = await response.json();
+
+        setLinkToken(data.link_token);
+    }
+
+    useEffect(() => {
+        generateToken();
+    }, []);
+
+    return linkToken != null ? <Link linkToken={linkToken} /> : <></>;
+}
+
+// LINK COMPONENT
+// Use Plaid Link and pass link token and onSuccess function
+// in configuration to initialize Plaid Link
+interface LinkProps {
+    linkToken: string | null;
+}
+
+const Link: React.FC<LinkProps> = (props: LinkProps) => {
+    const onSuccess = React.useCallback( async (public_token, metadata) => {
+        // send public_token to server
+        const response = await fetch(`${API_ENDPOINT}/plaid/exchange_public_token`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ public_token }),
+        });
+        const responseJson = await response.json();
+        console.log(responseJson);
+        
+        // Handle response ...
+        //TODO: save data locally
+    }, []);
+
+    const config: Parameters<typeof usePlaidLink>[0] = {
+        token: props.linkToken!,
+        onSuccess,
+    };
+
+    const { open, ready } = usePlaidLink(config);
+
+    return (
+        <button onClick={() => open()} disabled={!ready}>
+            Link account
+        </button>
+    );
+};
