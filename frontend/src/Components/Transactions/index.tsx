08609b5658fe4d6316e1208c2f8deec801441c73
agraham02 <acgraham02@gmail.com>
2024-01-20 14:29:35 -0500

    Add assets and liabilities section

--- frontend/src/Components/Transactions/index.tsx
+++ frontend/src/Components/Transactions/index.tsx
@@ -1,21 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { getRequest } from "../../utils";
+import { formatMoney, getRequest } from "../../utils";
 
 export default function Transactions() {
     const [accountData, setAccountData] = useState([]);
+    const [cash, setCash] = useState(0);
+    const [savings, setSavings] = useState(0);
+    const [investments, setInvestments] = useState(0);
+    const [debt, setDebt] = useState(0);
+    const [assets, setAssets] = useState(0);
+    const [liabilities, setLiabilities] = useState(0);
 
     async function fetchTransactions() {
         const results = await getRequest("/plaid/api/accounts");
         console.log(results.data);
         setAccountData(results.data.accounts);
+        calculateAssetsValues();
+    }
+
+    function calculateAssetsValues() {
+        const CHECKING = "checking";
+        const SAVINGS = "savings";
+        const INVESMENTS = "investments";
+        const CREDIT_CARD = "credit card";
+
+        let newCash = 0;
+        let newSavings = 0;
+        let newInvestments = 0;
+        let newDebt = 0;
+        for (const account of accountData) {
+            switch (account.subtype) {
+                case CHECKING:
+                    newCash += account.balances.available;
+                    break;
+                case SAVINGS:
+                    newSavings += account.balances.available;
+                    break;
+                case INVESMENTS:
+                    newInvestments += account.balances.available;
+                    break;
+                case CREDIT_CARD:
+                    newDebt += account.balances.current;
+                    break;
+            }
+        }
+
+        setCash(newCash);
+        setSavings(newSavings);
+        setInvestments(newInvestments);
+        setDebt(newDebt);
+        setAssets(newCash + newSavings + newInvestments);
+        setLiabilities(newDebt);
     }
 
     useEffect(() => {
         fetchTransactions();
     }, []);
 
+    useEffect(() => {
+        calculateAssetsValues();
+    }, [accountData]);
+
     return (
         <div>
+            <div>
+                <p>Stats:</p>
+                <div>Cash: {formatMoney(cash)}</div>
+                <div>Savings: {formatMoney(savings)}</div>
+                <div>Investments: {formatMoney(investments)}</div>
+                <div>Debt: {formatMoney(debt)}</div>
+                <div>Assets: {formatMoney(assets)}</div>
+                <div>Liabilities: {formatMoney(liabilities)}</div>
+                <div>Net worth: {formatMoney(assets - liabilities)}</div>
+            </div>
             {accountData.map((accountData, index) => (
                 <AccountCard key={index} accountData={accountData} />
             ))}
@@ -29,8 +85,8 @@ function AccountCard({ accountData }) {
             <div>
                 {accountData.name} {accountData.subtype}
             </div>
-            <div>Available Balance: {accountData.balances.available}</div>
-            <div>Current Balance: {accountData.balances.current}</div>
+            <div>Available Balance: {formatMoney(accountData.balances.available)}</div>
+            <div>Current Balance: {formatMoney(accountData.balances.current)}</div>
         </div>
     );
 }
--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -20,3 +20,25 @@ export async function deleteRequest(path: string, edpoint = API_ENDPOINT) {
     const results = await axios.delete(`${edpoint}${path}`, {withCredentials: true});
     return results;
 }
+
+export function formatDate(dateString: string) {
+    const date = new Date(dateString);
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1; // Months are zero-indexed
+    let day = date.getDate();
+
+    // Format the month and day to ensure they are two digits
+    const monthString = month < 10 ? "0" + month : month;
+    const dayString = day < 10 ? "0" + day : day;
+
+    // Combine the parts into a formatted date
+    let formattedDate = `${monthString}-${dayString}-${year}`;
+    return formattedDate;
+}
+
+export function formatMoney(amount: number, currency = "USD") {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: currency,
+    }).format(amount);
+}
